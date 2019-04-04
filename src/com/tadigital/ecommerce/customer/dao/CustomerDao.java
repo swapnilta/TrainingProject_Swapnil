e8dfbc0047cdeec0a9cd66696492974164533049
Swapnil Jain <Swapnil.Jain@tadigital.com>
2019-04-04 13:03:02 +0530

    new customer dao

--- src/com/tadigital/ecommerce/customer/dao/CustomerDao.java
+++ src/com/tadigital/ecommerce/customer/dao/CustomerDao.java
@@ -1,49 +1,94 @@
 package com.tadigital.ecommerce.customer.dao;
-import com.tadigital.ecommerce.customer.entity.*;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.Calendar;
-import java.util.GregorianCalendar;
 
 import com.tadigital.ecommerce.customer.entity.Customer;
 
-
-public class CustomerDao extends Dao 
-{
-
-	//validate email password
-	public boolean selectCustomerByEmailAndPassword(Customer customer)
-	{
-		boolean status=false;
+public class CustomerDao extends Dao {
+	public boolean selectCustomerByEmailAndPassword(Customer customer) {
+		boolean status = false;
 		
-		Connection con=openConnection();
-		Statement stmt=openStatement(con);
-		ResultSet rs=null;
+		Connection con = openConnection();
+		Statement stmt = openStatement(con);
+		ResultSet rs = null;
 		
-		try
-		{
-
-			String sql = "SELECT * FROM cust_info WHERE cust_email = '" + customer.getEmail() + "' AND cust_pswd = '" + customer.getPswd() + "'";
-		
+		try {
+			String sql = "SELECT * FROM customer_info WHERE cust_email = '" + customer.getEmail() + "' AND cust_pwd = '" + customer.getPassword() + "'";
+			
 			rs = stmt.executeQuery(sql);
-			if(rs.next()) 
-				status = true;	
-			System.out.println("status   "+status);
-			return status;
-		}catch (SQLException sqle) {
+			if(rs.next()) {
+				status = true;
+				
+				customer.setFirstName(rs.getString(1));
+				customer.setLastName(rs.getString(2));
+			}
+		} catch (SQLException sqle) {
 			sqle.printStackTrace();
 		} finally {
-			closeStatement(stmt);
 			closeResultSet(rs);
+			closeStatement(stmt);
 			closeConnection(con);
 		}
 		
 		return status;
 	}
-
+	
+	public boolean selectCustomerByEmail(Customer customer) {
+		boolean status = false;
+		
+		Connection con = openConnection();
+		Statement stmt = openStatement(con);
+		ResultSet rs = null;
+		
+		try {
+			String sql = "SELECT * FROM customer_info WHERE cust_email = '" + customer.getEmail() + "'";
+			
+			rs = stmt.executeQuery(sql);
+			if(rs.next()) {
+				status = true;
+			}
+		} catch (SQLException sqle) {
+			
+			sqle.printStackTrace();
+		} finally {
+			closeResultSet(rs);
+			closeStatement(stmt);
+			closeConnection(con);
+		}
+		
+		return status;
+	}
+	
+	
+	public boolean updatePassword(Customer customer) {
+		boolean status = false;
+		
+		Connection con = openConnection();
+		Statement stmt = openStatement(con);
+		
+		try {
+			String sql = "UPDATE customer_info SET cust_pwd ='" +customer.getPassword() +"' WHERE cust_email = '" + customer.getEmail() + "'";
+			
+			int row = stmt.executeUpdate(sql);
+			if(row != 0) {
+				status = true;
+			}
+		} catch (SQLException sqle) {
+			
+			sqle.printStackTrace();
+		} finally {
+			closeStatement(stmt);
+			closeConnection(con);
+		}
+		
+		return status;
+	}
+	
+	
+	
 	public boolean insertCustomer(Customer customer) {
 		boolean status = false;
 		
@@ -51,8 +96,9 @@ public class CustomerDao extends Dao
 		Statement stmt = openStatement(con);
 		
 		try {
-			String sql = "INSERT INTO cust_info(cust_fname,cust_lname, cust_email, cust_pswd) " +
-						 "VALUES('" + customer.getfName() + "', '"+customer.getlName()+  "', '"+ customer.getEmail() + "', '" + customer.getPswd() + "')";
+			String sql = "INSERT INTO customer_info(cust_fname, cust_lname, cust_email, cust_pwd) " +
+						 "VALUES('" + customer.getFirstName() + "', '" + customer.getLastName() +
+						 "', '" + customer.getEmail() + "', '" + customer.getPassword() + "')";
 			
 			int rows = stmt.executeUpdate(sql);
 			if(rows != 0) {
@@ -67,6 +113,4 @@ public class CustomerDao extends Dao
 		
 		return status;
 	}
-	
-	
 }
